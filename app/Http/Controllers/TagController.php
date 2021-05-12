03260167b8183b3403414c5fe1b481f0364f6cf0
atmjumayevilxom <jumayevilxom16@gmail.com>
2021-05-12 06:58:45 +0500

    postTag and tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\Tag;
 use Illuminate\Http\Request;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\TagResource;
 use Illuminate\Support\Facades\Validator;
 
-class UserController extends Controller
+class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -16,8 +16,19 @@ class UserController extends Controller
      */
     public function index()
     {
-        return response(UserResource::collection(User::all()), 200);
+        return response(TagResource::collection(Tag::all()), 200);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -28,61 +39,71 @@ class UserController extends Controller
     {
         $validate = Validator::make($request->toArray(), [
             'name' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-            'avatar' => 'required'
+            'slug' => 'required',
+            'parent_id' => 'required|exists:tags',
+
         ]);
 
         if ($validate->fails()) {
             return response($validate->errors(), 400);
         }
-        return response(new UserResource(User::create($validate->validate())), 201); // 201 Created
+        return response(new TagResource(Tag::create($validate->validate())), 201); // 201 Created
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Tag  $tag
      * @return \Illuminate\Http\Response
      */
-    public function show(User $user, $id)
+    public function show(Tag $tag)
     {
-        return response(new UserResource($user), 200);
+        return response(new TagResource($tag), 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tag $tag)
+    {
+        //
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\Tag  $tag
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $user, $id)
+    public function update(Request $request, Tag $tag)
     {
         $validate = Validator::make($request->toArray(), [
             'name' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-            'avata' => 'required'
+            'slug' => 'required',
+            'parent_id' => 'required|exists:tags',
         ]);
 
         if ($validate->fails()) {
             return response($validate->errors(), 400);
         }
 
-        $user->update($validate->validate());
-        return response(new UserResource($user), 201);
+        $tag->update($validate->validate());
+        return response(new TagResource($tag), 201);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Tag  $tag
      * @return \Illuminate\Http\Response
      */
-    public function destroy(User $user, $id)
+    public function destroy(Tag $tag)
     {
-        $user->delete();
+        $tag->delete();
 
         return response(null, 204);
     }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,12 @@
 <?php
-use illuminate\support\Facades\Route;
 
+use App\Http\Controllers\PostTagController;
+use App\Http\Controllers\TagController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::apiResources([
-    'Users'=> UserController::class
+    'Users' => UserController::class,
+    'Tags' => TagController::class,
+    'PostTag' => PostTagController::class
 ]);
-
-// Route::get('/users', [UserController::class, 'index']); // korinishi  barchasini
-// Route::post('/users', [UserController::class, 'store']); // qoshish creat
-// Route::get('/users/{id}', [UserController::class, 'show']); // bita id  korinishi
-// Route::put('/users/{id}', [UserController::class, 'update']);  // edit
-// Route::delete('/users/{id}', [UserController::class, 'destroy']); // ochirish
