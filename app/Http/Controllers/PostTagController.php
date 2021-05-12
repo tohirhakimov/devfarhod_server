73bff0eb9f398f52e2d0539abdcdfbb41abaf4ac
Farkhod Allamuradov <farkhod5allamuradov@gmail.com>
2021-05-12 16:44:09 +0500

    Update main

--- app/Http/Controllers/PostTagController.php
+++ app/Http/Controllers/PostTagController.php
@@ -20,16 +20,6 @@ class PostTagController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -40,8 +30,6 @@ class PostTagController extends Controller
         $validate = Validator::make($request->toArray(), [
             'post_id' => 'required|exists:posts',
             'tag_id' => 'required|exists:tags',
-
-
         ]);
 
         if ($validate->fails()) {
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TranslationController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::apiResources([
-    'translations' => TranslationController::class
+    'translations' => TranslationController::class,
+    'categories' => CategoryController::class,
+    'users' => UserController::class,
+    'posts' => PostController::class
 ]);
