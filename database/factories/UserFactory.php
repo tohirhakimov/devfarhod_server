d1d9a9163bb72da76a77ec39a8f08058b307cfc6
atmjumayevilxom <jumayevilxom16@gmail.com>
2021-05-14 04:18:04 +0500

    user add seed and faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->text(20),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('secret'),
+            'avatar' => $this->faker->text(50),
             'remember_token' => Str::random(10),
         ];
     }
