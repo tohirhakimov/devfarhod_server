a83f7e0717a75c3896644f3656f79b9886d1f549
tohirhakimov <hakimovtohirbek@gmail.com>
2021-05-20 14:40:21 +0500

    create-factory-and-seeder-for-post-categories-table

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $names = $this->faker->word();
+        $category = Category::get()->count();
+        return [
+            'name'=> $names,
+            'slug'=>Str::slug($names),
+            'parent_id' => ($category > 1) ? rand(1, $category) : null,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,9 @@ use App\Models\Post;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Tag;
-// use App\Models\PostTag;
+use App\Models\Category;
+use App\Models\PostCategory;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,11 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->has(
-            Post::factory()->has(
-                Tag::factory()->count(4)
-            )->count(5)
-        )->create();
-        // PostTag::factory(50)->create();
+        // User::factory(10)->has(
+        //     Post::factory()->has(
+        //         Tag::factory()->count(4)
+        //     )->count(5)
+        // )->create();
+
+        // Category::factory(30)->create();
+
+        Post::factory()->has(
+            PostCategory::factory(
+            )->count(3)
+        )->create();    
     }
 }
