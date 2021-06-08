90f81ccff7ce55b1e1420eadfb10e7584b0b98c2
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-06-08 14:02:35 +0500

    Add tag routes

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TranslationController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use App\Models\Tag;
 
 Route::apiResources([
     'translations' => TranslationController::class,
@@ -12,4 +13,14 @@ Route::apiResources([
     'posts' => PostController::class
 ]);
 
-Route::get('/', );
+Route::get('/tags', function () {
+    return response()->json(Tag::all(), 200);
+});
+
+Route::get('/tags/{tag}', function ($id) {
+    return response()->json(Tag::find($id), 200);
+});
+
+Route::post('/tags', function (Request $request) {
+    return Tag::create($request->all());
+});
