57b252e11de69b30bf0ae8a519dd1aeff2cce56a
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-06-23 11:16:47 +0500

    Update TagResource.php

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Tag;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TagResource extends JsonResource
@@ -14,19 +15,27 @@ class TagResource extends JsonResource
      */
     public function toArray($request)
     {
+        $parent = Tag::find($this->parent_id);
+        $parentData = [];
+        if (!is_null($parent)) {
+            $parentData = [
+                'id' => $parent->id,
+                'name' => $parent->name,
+                'slug' => $parent->slug,
+                'created_at' => $parent->created_at,
+                'updated_at' => $parent->updated_at,
+                'deleted_at' => $parent->deleted_at
+            ];
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
-            'parent_id' => $this->parent_id,
-
-        ];
-    }
-    public function with($request)
-    {
-        return [
-            'version' => '1.0.2',
-            'author' => 'Post'
+            'parent' => $parentData,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at
         ];
     }
 }
